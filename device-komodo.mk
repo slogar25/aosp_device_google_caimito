1a541b00a0bbbac89f990e7fd0c6e36f771e02f7
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-08-26 19:16:25 +0300

    add FIR proximity sensor feature to caiman and komodo

--- device-komodo.mk
+++ device-komodo.mk
@@ -447,3 +447,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.bluetooth.thread_dispatcher.enabled=true
+
+PRODUCT_VENDOR_PROPERTIES := $(filter-out ro.vendor.build.svn=% , $(PRODUCT_VENDOR_PROPERTIES))
+
+PRODUCT_PACKAGES += fir-proximity-feature.xml
