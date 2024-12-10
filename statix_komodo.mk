7c636c53a69fbd96b79b75e6ba2143aa6c622115
slogar25 <michaelslogar@gmail.com>
2024-12-10 22:38:19 +0000

    komodo: add statix make file

---
+++ statix_komodo.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (c) 2024 StatiXOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common StatiX stuff.
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/caimito/aosp_komodo.mk)
+
+# Parts
+$(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
+$(call inherit-product-if-exists, vendor/google/pixelparts/powershare/device.mk)
+$(call inherit-product-if-exists, vendor/google/pixelparts/face/face.mk)
+
+# Pixel Launcher
+INCLUDE_PIXEL_LAUNCHER := true
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := statix_komodo
+PRODUCT_MODEL := Pixel 9 Pro XL
+PRODUCT_BRAND := google
+PRODUCT_MANUFACTURER := Google
+
+PRODUCT_OVERRIDE_FINGEPRINT := "google/komodo/komodo:15/AP4A.241205.013/12621605:user/release-keys"
+
+BUILD_FINGERPRINT := $(PRODUCT_OVERRIDE_FINGEPRINT)
